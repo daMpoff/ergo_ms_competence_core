abc859ca657908cd5bf525e063ceb91b8196b278
Wyaro <eremin_igor_2004@mail.ru>
2025-11-25 09:25:10 +0300

    feat: расширение словаря технологий для инициализации

--- api/skill_map/data/__init__.py
+++ api/skill_map/data/__init__.py
@@ -1,8 +1,45 @@
 """
 Модуль данных для первичной инициализации skill_map.
+
+Содержит справочники технологий, разбитые по категориям:
+- languages.py - Языки программирования
+- frameworks_backend.py - Backend фреймворки
+- frameworks_frontend.py - Frontend фреймворки
+- databases.py - Базы данных
+- devops.py - DevOps инструменты
+- data_science.py - Data Science и ML
+- mobile.py - Мобильная разработка
+- testing.py - Тестирование
+- cloud_platforms.py - Облачные платформы
+- protocols.py - Протоколы и очереди сообщений
 """
 
-from modules.competence_core.api.skill_map.data.technologies_dict import ALL_TECHNOLOGIES
+from .technologies_dict import (
+    ALL_TECHNOLOGIES,
+    PROGRAMMING_LANGUAGES,
+    BACKEND_FRAMEWORKS,
+    FRONTEND_FRAMEWORKS,
+    DATABASES,
+    DEVOPS_TOOLS,
+    DATA_SCIENCE_LIBRARIES,
+    MOBILE_DEVELOPMENT,
+    TESTING_TOOLS,
+    CLOUD_PLATFORMS,
+    PROTOCOLS,
+    get_statistics,
+)
 
-__all__ = ['ALL_TECHNOLOGIES']
-
+__all__ = [
+    'ALL_TECHNOLOGIES',
+    'PROGRAMMING_LANGUAGES',
+    'BACKEND_FRAMEWORKS',
+    'FRONTEND_FRAMEWORKS',
+    'DATABASES',
+    'DEVOPS_TOOLS',
+    'DATA_SCIENCE_LIBRARIES',
+    'MOBILE_DEVELOPMENT',
+    'TESTING_TOOLS',
+    'CLOUD_PLATFORMS',
+    'PROTOCOLS',
+    'get_statistics',
+]
